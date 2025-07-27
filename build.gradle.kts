64652ef312c9089a71cc8b1e66086415c0b8a0f7
stualyttle <stualyttle@users.noreply.github.com>
2025-07-27 19:14:46 +0200

    Update lytleutils dependency version to 1.1.7

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,7 +30,7 @@ repositories {
 
 dependencies {
     compileOnly("io.papermc.paper:paper-api:1.21.8-R0.1-SNAPSHOT")
-    implementation("com.lyttledev:lyttleutils:1.1.4")
+    implementation("com.lyttledev:lyttleutils:1.1.7")
 }
 
 group = "com.lyttledev"
